0d87610e5d53644c6daadc948091fe5ea44b49d5
linzhen <273487984@qq.com>
2017-05-15 10:42:19 +0800

    twin prime problem

--- twin_prime/twin_prime.cpp
+++ twin_prime/twin_prime.cpp
@@ -1,30 +1,48 @@
 #include <iostream>
 #include <stdio.h>
 using namespace std;
-
+const int N=1000000+10;
+int prime[N];
 bool isPrime(int n)
 { // 判断是否为素数
-    if (n < 2) {
-        return false;
+    if (!prime[n]) { //记录是否为素数
+        if (n < 2) {
+            prime[n] = 2;// 非素数
+        } else {
+            for(int i = 2; i*i <= n; i++) {
+                if (n%i == 0) {
+                    prime[n] = 2;
+                    break;
+                }
+            }
+            if (!prime[n]) {
+                prime[n] = 1;
+            }
+        }  
     }
-    for(int i = 2; i*i< n; i++) {
-        if (n%i == 0) {
-            return false;
-        }
-    }
-    return true;
+    return prime[n] == 1;// 1为素数 2为非素数
 }
 
 int main()
 {
+    for (int i = 1;i <= N; i++) {
+        prime[i] = 0;
+    }
     int m,n,i,j,count,res[10000];
     scanf("%d", &m);
 
     for (i = 0; i < m; i++) {
         scanf("%d", &n);
+        if (n == 1 || n == 2) { // 1,2,3 特殊
+            res[i] = 0;
+            continue;
+        } else if(n == 3) {
+            res[i] = 1;
+            continue;
+        }
         j = 3;
         count = 1;
-        while(j <= n) {
+        while(j <= n-2) {
             if (isPrime(j) && isPrime(j+2)) {
                 count ++;
             }
@@ -35,4 +53,5 @@ int main()
     for(i = 0; i < m;i++) {
         printf("%d\n", res[i]);
     }
+    return 0;
 }
