c341fce4e483fbd2fbd45d299cbb9513252dcdad
linzhen <273487984@qq.com>
2017-05-22 13:27:07 +0800

    complete 6174 problem

--- 6174/6174.cpp
+++ 6174/6174.cpp
@@ -5,25 +5,39 @@ bool cmp(int a,int b)
 {
     return a>b;
 }
-int convertNum(int n[]) {
+int convertNum(int n[]) // 数组转为数字
+{
     return n[0] * 1000 + n[1] * 100 + n[2] * 10 + n[3];
 }
-
+void convertArr(int n, int *arr) // 数字转为数组
+{
+    for(int i = 0; i < 4; i++) {
+        arr[i] = 0;
+    }
+    arr[0] = n/1000;
+    arr[1] = (n - convertNum(arr)) / 100;
+    arr[2] = (n - convertNum(arr)) / 10;
+    arr[3] = n - convertNum(arr);
+}
 int main()
 {
-    string a = "3241";
-    int min,max,b[4];
-    
-    for(int i = 0; i < 4; i ++) {
-        b[i] = atoi((a.substr(i, 1)).c_str());
+    int a,n,min,max,count,b[4];
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> a;
+        min = 0;
+        max = a;
+        count = 0;
+        do {
+            a = max - min;
+            convertArr(a, b);
+            sort(b, b+4);// 升序排序
+            min = convertNum(b);
+            sort(b, b+4, cmp);// 降序排序
+            max = convertNum(b);
+            count ++;
+        } while ((max - min) != a);// 循环至相等
+
+        cout << count << endl;
     }
-    sort(b, b+4);
-    int min = convertNum(b);
-    sort(b, b+4, cmp);
-    int max = convertNum(b);
-    cout << min << endl;
-    cout << max << endl;
-    // int a[4] = {4,3,2,1};
-    // sort(a, a+4);
-    // cout<<a[1];
 }
